3ca53895cca873e698f963babcfd3dc3f960a9d6
Albert Diones <adiones@addteam.org>
2024-09-02 18:21:56 +0800

    implement order handler on exchange

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
 import XhrJson from "./xhrjson";
 import {Logger} from "add_logger"
 import type {rawExchangeCandle, TickerData } from "./tradingCandles";
+import {type OrderHandler} from "tradeOrders/orderHandler";
 
-export interface Exchange {
+export interface Exchange extends OrderHandler {
   fetchCandlesFromExchange(symbol: string, minutes: number, limit: number): Promise<rawExchangeCandle[] | null>;
   getAssets(): Promise<string[]>;
   getTickerData(symbol: string): Promise<{data: TickerData,fromCache: Boolean} | null>;
