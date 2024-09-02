2a3855fbbe4ba74a909d65bf363b7dfdcf497cee
Albert Diones <adiones@addteam.org>
2024-09-02 18:14:28 +0800

    Revert "stash: 4 interface idea: assetWallet, tickerFetcher, candleFetcher, orderHandler"
    This reverts commit 7f713feb2627f1500cc35919bbd12ee7f85df5cb.

--- index.ts
+++ index.ts
@@ -1,36 +1,11 @@
 import XhrJson from "./xhrjson";
 import {Logger} from "add_logger"
 import type {rawExchangeCandle, TickerData } from "./tradingCandles";
-import {type OrderHandler} from "tradeOrders/orderHandler";
 
-export interface Exchange extends AssetWallet, TickerFetcher, CandleFetcher, OrderHandler {
-  
-}
-
-export interface AssetWallet {  
-  getAssets(): Promise<string[]>; // ['BTC','XRP']
-  fetchWallet(): Array<[string, string, number, number, number, string, object]> {    
-    return this._fetch('/v2/auth/r/wallets', {})
-    .then((result) => {
-        return result;
-    });
-}
-}
-
-export interface TickerFetcher {
-  
-  getTickers(): Promise<string[]>; // ['BTC','XRP']
-
-  getTickerData(symbol: string): Promise<{data: TickerData,fromCache: Boolean} | null>;
-  getDefaultTickerSymbol(baseAsset: string): string | null;
-}
-
-
-
-export interface CandleFetcher { 
-  
+export interface Exchange {
   fetchCandlesFromExchange(symbol: string, minutes: number, limit: number): Promise<rawExchangeCandle[] | null>;
-  
+  getAssets(): Promise<string[]>;
+  getTickerData(symbol: string): Promise<{data: TickerData,fromCache: Boolean} | null>;
   _candleCountFromCloseTimestamp(timestamp: number, minutes: number): number; // todo: figure out if to move or remove this
-
+  getUsdtSymbol(baseAsset: string): string | null;
 }
