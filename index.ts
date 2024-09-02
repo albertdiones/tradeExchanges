9a37d1bae4bc6d031587427257846ffbdc3bc088
Albert Diones <adiones@addteam.org>
2024-09-02 18:32:14 +0800

    AssetWallet interface

--- index.ts
+++ index.ts
@@ -3,10 +3,26 @@ import {Logger} from "add_logger"
 import type {rawExchangeCandle, TickerData } from "./tradingCandles";
 import {type OrderHandler} from "tradeOrders/orderHandler";
 
-export interface Exchange extends OrderHandler {
+export interface Exchange extends AssetWallet, OrderHandler {
   fetchCandlesFromExchange(symbol: string, minutes: number, limit: number): Promise<rawExchangeCandle[] | null>;
-  getAssets(): Promise<string[]>;
   getTickerData(symbol: string): Promise<{data: TickerData,fromCache: Boolean} | null>;
   _candleCountFromCloseTimestamp(timestamp: number, minutes: number): number; // todo: figure out if to move or remove this
   getUsdtSymbol(baseAsset: string): string | null;
+}
+
+export interface AssetHolding {
+  name: string;
+  amount: number;
+}
+
+export interface AssetWallet {
+  /**
+   * Get all the assets supported by the platform(exchange?)
+   */
+  getSupportedAssets(): Promise<string[]>;
+
+  /**
+   * Fetches all the holdings, name and value from the platform
+   */
+  fetchHoldings(): AssetHolding[];
 }
